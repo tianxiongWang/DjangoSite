5f37823d374225eec9609b83aa064cd621bae3de
wangxiong <13961252501@139.com>
2019-08-03 12:54:16 +0800

    总算把最近一直思考的按年月分类统计完成了

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,25 @@ def blog_list(request):
     categories = Category.objects.all()
     blogs = Blog.objects.all()
     blogs_see = blogs.order_by('-see')
+    # 设置不重复的年月类别，并传送到前端模板
+    YearMonth = set()
     for blog in blogs:
+        YearMonth.add((blog.published.year, blog.published.month))
+    counter = {}.fromkeys(YearMonth, 0)
+    for blog in blogs:
+        counter[(blog.published.year, blog.published.month)] += 1
         blog.published = blog.published.strftime("%Y-%m-%d %H:%I:%S")
+    #这个是传递到前端的最终数据
+    yearMonth = []
+    for count in counter:
+        yearMonth.append(("%s年%s月" % (count), counter[count]))
+    print(yearMonth)
     context = {}
     context['blogs'] = blogs
     context['categories'] = categories
+    # 最受欢迎的五篇博客
     context['blogs_see'] = blogs_see[0:5]
+    context['yearMonth'] = yearMonth
     return render(request, 'blog.html', context=context)
 
 
