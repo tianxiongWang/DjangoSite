280998d7eec05b5da1775bba064383ee14799438
wangxiong <13961252501@139.com>
2019-08-08 08:50:05 +0800

    新增用户内容并设置一对一

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    mobile = models.CharField(max_length=11)
+    email = models.EmailField()
+    nickname = models.OneToOneField(User, on_delete=models.CASCADE)
