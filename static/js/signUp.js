68b39c421c7fb8b4c50bd0b34b50ca0925581616
tianxiongWang <13961252501@139.com>
2019-08-10 12:03:44 +0800

    注册界面的js写好了第一个，后面的可以很快就写好了

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -1,9 +1,35 @@
 var username = document.getElementById("username");
-// 离开焦点，将触发ajax请求，判断用户是否重复
-username.onblur = function () {
-    ajaxUsername();
+var password = document.getElementById("password");
+var password_repeat = document.getElementById("password_repeat");
+var mobile = document.getElementById("mobile");
+var mobile_identy = document.getElementById("mobile_identy");
+var email = document.getElementById("email");
+var nickname = document.getElementById("nickname");
+var QQ = document.getElementById("QQ");
+var check1 = 0,
+    check2 = 0,
+    check3 = 0,
+    check4 = 0,
+    check5 = 0,
+    check6 = 0,
+    check7 = 0,
+    check8 = 0;
+
+window.onload = function () {
+    username.onblur = function () {
+        var patrn = /[a-zA-Z0-9._/]{5,20}/;
+        var usr = username.valuel;
+        check1 = patrn.test(usr);
+        if (check1 == true) {
+            document.getElementsByClassName("check")[0].innerHTML = "<span></span>&nbsp;该用户名合法";
+            document.getElementsByClassName("check")[0].children[0].setAttribute("class", "glyphicon glyphicon-ok");
+            document.getElementsByClassName("check")[0].children[0].setAttribute("style", "color:green;");
+        }
+    }
 }
 
-function ajaxUsername() {
-    alert('1234');
+
+function checkForm() {
+    alert(check1);
+    return false;
 }
